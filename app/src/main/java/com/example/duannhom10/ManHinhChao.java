32fe161221576c07d20a8deafe44eee7309e9f7e
manh <manhtvph22328@fpt.edu.vn>
2024-03-16 22:47:41 +0700

    #1-manhtv layout

---
+++ app/src/main/java/com/example/duannhom10/ManHinhChao.java
@@ -0,0 +1,15 @@
+package com.example.duannhom10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManHinhChao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_chao);
+//        Ahihihi
+    }
+}
